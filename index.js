db4fd995a742be0cb957c465575b366623fcd432
jscodelover <manisha.4918@gmail.com>
2019-03-27 00:24:28 +0530

    feat: d list for resume field

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const inquirer = require("inquirer");
 const chalk = require("chalk");
+const resume = require("./resume.json");
 
 const resumeContext = chalk.bold.whiteBright;
 const WelcomeMessage = "Hey developer, Welcome to my resume !!!!";
 
 function main() {
   console.log(resumeContext(WelcomeMessage));
+  resumeHandler();
+}
+
+function resumeHandler() {
+  inquirer
+    .prompt({
+      type: "list",
+      message: "What do you want know about me?",
+      name: "resumeField",
+      choices: [...Object.keys(resume), "Exit"]
+    })
+    .then(answers => {
+      console.log(answers);
+    });
 }
 
 main();
